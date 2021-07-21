0b3e37120843b17ff8f80af5f8cc4242f786d9e7
Sarthak Gupta <sarthakgupta878@gmail.com>
2021-07-21 12:37:40 +0530

    first commit

---
+++ Learning web/Basic Topics/53_functions.js
@@ -0,0 +1,28 @@
+console.log("this is tut 53");
+function greet(names,greettext="hfhjh"){
+console.log( names + " is a good boy");
+console.log( greettext + names  );
+
+    
+}
+
+function sum(a,b,c)
+{
+    let d=a+b+c;
+    return d;
+}
+
+let name1="sarthak";
+let name2="sarthak2";
+let name3="sarthak3";
+let name4="sarthak4";
+
+let greettext ="good morning ";
+// console.log( name1 + " is a good boy");
+greet(name1,greettext);
+greet(name2,greettext);
+greet(name3,greettext);
+greet(name4);
+
+let ret=sum(1,2,3);
+console.log(ret);
