90dc604b3578fcba116a1fc9e4608f93dc3f8869
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-02-19 16:03:18 +0530

    compleated clock2

---
+++ Projects using html,css,js/011_clockjs/index.js
@@ -0,0 +1,24 @@
+console.log("This is clock.js");
+
+function updateClock(){
+    let currentTime = new Date();
+    let currentHour =currentTime.getHours();
+    let currentMinutes =currentTime.getMinutes();
+    let currentSeconds =currentTime.getSeconds();
+
+    currentMinutes = (currentMinutes<10 ? "0":"")+currentMinutes;
+    currentSeconds = (currentSeconds<10 ? "0":"")+currentSeconds;
+    
+    currentHour = (currentHour>12) ? (currentHour-12) : currentHour;
+    currentHour = (currentHour==0) ? 12 : currentHour;
+
+    
+    currentHour = (currentHour<10 ? "0":"")+currentHour;
+
+    let timeofDay = (currentHour<12) ? "AM":"PM";
+
+    let currentTimeStr = currentHour + ":" +currentMinutes +":"+ currentSeconds +" "+timeofDay;
+
+
+    document.getElementById('clock').innerHTML = currentTimeStr;
+}
