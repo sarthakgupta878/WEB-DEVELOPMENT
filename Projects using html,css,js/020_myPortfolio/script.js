77a678f4d8337e150eadbba637ab0b47f1b19ee0
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-03-18 17:33:22 +0530

    compleated my first portfolio

---
+++ Projects using html,css,js/020_myPortfolio/script.js
@@ -0,0 +1,25 @@
+// console.log("hi")
+
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", () => {
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+
+    if (document.querySelector('.sidebar').classList.contains('sidebarGo')) {
+        setTimeout(() => {
+            document.querySelector('.ham').style.display = 'inline';
+            
+        }, 250);
+        document.querySelector('.cross').style.display = 'none';
+
+    } else {
+        document.querySelector('.ham').style.display = 'none';
+        setTimeout(() => {
+            document.querySelector('.cross').style.display = 'inline';
+            
+        }, 100);
+
+    }
+
+})
+
+
